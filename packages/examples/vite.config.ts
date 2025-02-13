33034e837c58c0ac8f43b2488bc6aaa570d673cb
fwx5618177 <fengwenxuan2006@126.com>
2025-02-14 03:42:31 +0800

    update

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -4,19 +4,16 @@ import { defineConfig } from 'vite';
 // 示例应用构建配置
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/simulation-3D-City-performance/'
-      : '/',
+  base: '/simulation-3D-City-performance/',
   build: {
     emptyOutDir: true,
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
-      input: 'index.html',
       output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
       },
     },
   },
