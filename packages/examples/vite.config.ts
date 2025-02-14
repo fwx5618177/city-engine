3332e3dcd4c74b80f5ddef5001b5ade25729c7df
fwx5618177 <fengwenxuan2006@126.com>
2025-02-14 20:45:21 +0800

    update

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/simulation-3D-City-performance/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
